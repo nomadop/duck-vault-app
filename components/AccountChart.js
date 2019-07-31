b66cf3931a607a8f6e47749c52b83eeed8e2783d
nomako <nomadop001@gmail.com>
2019-07-31 11:55:26 +0800

    refactor account chart component

--- components/AccountChart.js
+++ components/AccountChart.js
@@ -9,11 +9,23 @@ import { currency } from '../helpers/Number';
 import SwitchButton from './SwitchButton';
 import Colors from '../constants/Colors';
 
+const chartBarStyle = {
+  data: { fill: Colors.tintColor },
+  labels: { fontSize: 10, fill: '#9b9b9b' }
+};
+
+const axisStyle = {
+  axis: { stroke: '#ccc' },
+  ticks: { stroke: "#ccc", size: 5 },
+  tickLabels: { fill: '#9b9b9b', padding: 3 },
+};
+
 export class AccountChart extends Component {
-  constructor() {
+  constructor(props) {
     super();
     this.state = {
       height: new Animated.Value(0),
+      activeSection: _.last(props.section),
     }
   }
 
@@ -25,34 +37,31 @@ export class AccountChart extends Component {
     return true;
   }
 
+  renderChart() {
+    const data = flow(take(9), reverse)(this.props.accounts);
+    return (
+      <VictoryChart height={270} padding={30} theme={VictoryTheme.grayscale}>
+        <VictoryBar data={data}
+                    y="total"
+                    x={({ title }) => flow(split('-'), takeRight(2), join('-'))(title)}
+                    style={chartBarStyle}
+                    labels={({ total }) => currency(total)}
+        />
+        <VictoryAxis style={axisStyle} fixLabelOverlap />
+      </VictoryChart>
+    )
+  }
+
   render() {
-    const { accounts, opened, onChangeType } = this.props;
-    const data = flow(take(9), reverse)(accounts);
+    const { opened, onChangeType } = this.props;
     const chartContainerStyle = { height: this.state.height };
-    const chartBarStyle = {
-      data: { fill: Colors.tintColor },
-      labels: { fontSize: 10, fill: '#9b9b9b' }
-    };
-    const axisStyle = {
-      axis: { stroke: '#ccc' },
-      ticks: { stroke: "#ccc", size: 5 },
-      tickLabels: { fill: '#9b9b9b', padding: 3 },
-    };
     return (
       <Animated.View style={[styles.chartContainer, chartContainerStyle]}>
         <SwitchButton defaultValue="month"
                       buttons={[{ title: '月', value: 'month' }, { title: '日', value: 'day' }]}
                       width={80} height={30} style={{ alignSelf: 'flex-start', marginLeft: 30, }}
                       onChange={onChangeType} />
-        {opened && <VictoryChart height={270} padding={30} theme={VictoryTheme.grayscale}>
-          <VictoryBar data={data}
-                      y="total"
-                      x={({ title }) => flow(split('-'), takeRight(2), join('-'))(title)}
-                      style={chartBarStyle}
-                      labels={({ total }) => currency(total)}
-          />
-          <VictoryAxis style={axisStyle} fixLabelOverlap />
-        </VictoryChart>}
+        {opened && this.renderChart()}
       </Animated.View>
     );
   }
